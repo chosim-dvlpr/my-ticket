441ab987db1263df9dc71f22a507d7bfebbd3330
bmj0053 <bmj05378@gmail.com>
2025-05-18 22:33:21 +0900

    chore: eslint 설정 및 파일 세미콜론 제거

---
+++ backend/src/entities/event.entity.ts
@@ -0,0 +1,31 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('event')
+export class Event {
+  @PrimaryGeneratedColumn('increment')
+  id: number;
+
+  @Column({ type: 'date' })
+  start_date: string;
+
+  @Column({ length: 255 })
+  event_name: string;
+
+  @Column({ length: 255, nullable: true })
+  poster_url: string;
+
+  @Column({ type: 'date' })
+  end_date: string;
+
+  @Column({ length: 25 })
+  place: string;
+
+  @Column({ length: 255 })
+  road_address: string;
+
+  @Column({ length: 255, nullable: true })
+  md_info_url: string;
+
+  @Column({ length: 255, nullable: true })
+  seating_image_url: string;
+}
