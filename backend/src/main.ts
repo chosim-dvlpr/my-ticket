d61c0090a08e5c77796e05be3951ad2cbbd5a5ac
bmj0053 <bmj05378@gmail.com>
2025-05-24 14:24:37 +0900

    [BE] feat: localhost CORS 설정

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,7 +4,10 @@ import { setupSwagger } from './utils/swagger'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-
+  app.enableCors({
+    origin: ['http://localhost:3000'],
+    credentials: true,
+  })
   setupSwagger(app)
 
   await app.listen(8080)
