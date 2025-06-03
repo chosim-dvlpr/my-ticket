2719dd752ae7221ca90a991542198776840141e6
bmj0053 <bmj05378@gmail.com>
2025-06-04 01:26:10 +0900

    [BE] feat: ecosystem config 파일 추가

---
+++ backend/ecosystem.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  apps: [
+    {
+      name: 'my-ticket', // PM2 프로세스 이름
+      script: './dist/main.js',
+      exec_mode: 'fork',
+      watch: false,
+      max_memory_restart: '1G',
+      env: {
+        NODE_ENV: 'production',
+      },
+    },
+  ],
+}
