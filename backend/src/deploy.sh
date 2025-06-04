5cbda6023e6fbabed0ad2a73e0529214c8ef76df
Minji <121149171+chosim-dvlpr@users.noreply.github.com>
2025-06-04 12:50:53 +0900

    CI/CD 설정 (#6)
    * feat: Frontend Dev CI 추가
    
    * fix: frontend node version을 20.15.1로 변경
    
    * fix: node version을 22.15.0으로 변경 및 env 추가
    
    * fix: [FE] env 위치 변경
    
    * [BE] feat: 백엔드 CI 작성
    
    * [BE] feat: ecosystem config 파일 추가
    
    * [BE] feat: CD 코드 추가
    
    * [BE] feat: deploy script, nvmrc 파일 추가
    
    * [BE] feat: 백엔드 CD AWS auth 연결
    
    * [BE] fix: deploy.sh 임시 보완
    
    * [BE] fix: ecosystem 임시 보완
    
    * [BE] fix: 타겟 브랜치 변경

---
+++ backend/src/deploy.sh
@@ -0,0 +1,18 @@
+set -e # 에러 발생 시 스크립트 종료
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+nvm install
+nvm use
+
+cd ~/my-ticket/backend
+
+git pull origin main
+
+yarn cache clean
+yarn install --frozen-lockfile
+
+yarn build
+
+pm2 restart ecosystem.config.js
