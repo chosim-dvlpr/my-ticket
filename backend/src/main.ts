70a6133d79388db1cf48d2d090cd63c293e4c950
bmj0053 <bmj05378@gmail.com>
2025-07-17 01:57:43 +0900

    [BE] chore: cors 설정 추가 (vercel)

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,8 +4,18 @@ import { setupSwagger } from './utils/swagger'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+  const getAllowedOrigins = () => {
+    const baseOrigins = ['http://localhost:3000']
+
+    if (process.env.NODE_ENV === 'production') {
+      return [...baseOrigins, 'https://my-ticket-frontend.com', 'https://my-ticket-frontend.vercel.app']
+    }
+
+    return [...baseOrigins, 'https://my-ticket-frontend-git-*.vercel.app']
+  }
+
   app.enableCors({
-    origin: ['http://localhost:3000'],
+    origin: getAllowedOrigins(),
     credentials: true,
   })
   setupSwagger(app)
