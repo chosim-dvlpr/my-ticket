e9b50e1aadb37bd7445b7e1a86f2ade517666b5d
bmj0053 <bmj05378@gmail.com>
2025-07-24 14:22:39 +0900

    [FE] style: 카드 스타일 지정

--- frontend/src/app/events/[eventId]/page.tsx
+++ frontend/src/app/events/[eventId]/page.tsx
@@ -16,25 +16,27 @@ export default function EventDetailPage() {
   return (
     <div>
       {/* Event Card */}
-      <div className="border-2 border-gray-100">
+      <div className="border-2 border-gray-100 rounded-xl overflow-hidden mt-4">
         <div className="overflow-hidden w-full">
           {/* TODO: 기본 이미지 추가 */}
           <Image
             src={event.poster_url || 'https://example.com'}
-            className=""
-            width={500}
+            className="h-40 object-cover"
+            width={400}
             height={100}
             unoptimized
             alt="포스터 이미지"
           />
         </div>
-        <div className="">{event.event_name}</div>
-        <div className="flex gap-2 text-gray-400 mt-2">
-          <div>{event.start_date}</div>
-          <div>-</div>
-          <div>{event.end_date}</div>
+        <div className="p-4">
+          <div className="font-bold">{event.event_name}</div>
+          <div className="flex gap-2 text-gray-400 mt-2">
+            <div>{event.start_date}</div>
+            <div>-</div>
+            <div>{event.end_date}</div>
+          </div>
+          <div className="text-gray-400">{event.place}</div>
         </div>
-        <div className="text-gray-400">{event.place}</div>
       </div>
       {/* chip */}
       {/* Tickets */}
